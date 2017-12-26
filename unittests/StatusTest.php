1237dbcc0f265b5773db695d3ddafacb1050f75a
Andreas Heigl <andreas@heigl.org>
2017-12-26 22:09:38 +0100

    Creates mock-objects for status tests

--- unittests/StatusTest.php
+++ unittests/StatusTest.php
@@ -1,9 +1,9 @@
 <?php
 use PHPUnit\Framework\TestCase;
-require_once('config/config.php');
+require_once('config/config.dist.php');
 require_once 'class/metrolink.class.php';
 require_once 'class/status.class.php';
-require_once 'unittests/stubs/websitehandler.stub.php';
+require_once 'class/promises/websitehandler.interface.php';
 
 /**
  * @covers Metrolink
@@ -13,27 +13,84 @@ final class StatusTest extends TestCase
     public function setUp() {
     }
 
-    public function testGetStatusString()
+    /** @dataProvider getStatusStringsProvider */
+    public function testGetStatusString($datetime, $expected, $json)
     {
-        $websiteHandler = new WebsiteHandlerStub('GOOD_SERVICE');
-        $metrolink = new Metrolink(new DateTime('Monday 13:00'));
-        $s = new Status($websiteHandler, $metrolink);
-        $this->assertEquals('', $s->getStatusString(), "Good service on all lines, non-peak");
+        $website = $this->createMock(WebsiteHandlerInterface::class);
+        $website->method('getMetrolinkStatus')->willReturn($json);
 
-        $websiteHandler = new WebsiteHandlerStub('GOOD_SERVICE');
-        $metrolink = new Metrolink(new DateTime('Monday 9:00'));
-        $s = new Status($websiteHandler, $metrolink);
-        $this->assertEquals('9:00 — Good service on all lines', $s->getStatusString(), "Good service on all lines, peak");
+        $metrolink = new Metrolink($datetime);
+        $s = new Status($website, $metrolink);
+        $this->assertEquals($expected, $s->getStatusString());
+    }
 
-        $websiteHandler = new WebsiteHandlerStub('MINOR_DELAYS');
-        $metrolink = new Metrolink(new DateTime('Monday 13:00'));
-        $s = new Status($websiteHandler, $metrolink);
-        $this->assertEquals("Minor delays on East Didsbury line\nMinor delays on Rochdale via Oldham line\nGood service on all other lines",
-            $s->getStatusString(), "Minor delays, non-peak");
+    public function getStatusStringsProvider()
+    {
+        return [
+            'Good Service on all lines, non-peak' => [
+                new DateTimeImmutable('Monday 13:00'),
+                '',
+                $this->getGoodServiceData(),
+            ],
+            'Good service on all lines, peak' => [
+                new DateTimeImmutable('Monday 9:00'),
+                '9:00 — Good service on all lines',
+                $this->getGoodServiceData(),
+            ],
+            'Minor delays, non-peak' => [
+                new DateTimeImmutable('Monday 13:00'),
+                "Minor delays on East Didsbury line\nMinor delays on Rochdale via Oldham line\nGood service on all other lines",
+                $this->getMinorDelaysData(),
+            ],
+            'Minor delays, peak' => [
+                new DateTimeImmutable('Monday 17:00'),
+                "17:00 — Minor delays on East Didsbury line\nMinor delays on Rochdale via Oldham line\nGood service on all other lines",
+                $this->getMinorDelaysData(),
+            ],
+        ];
+    }
 
-        $websiteHandler = new WebsiteHandlerStub('MINOR_DELAYS');
-        $metrolink = new Metrolink(new DateTime('Monday 17:00'));
-        $s = new Status($websiteHandler, $metrolink);
-        $this->assertEquals("17:00 — Minor delays on East Didsbury line\nMinor delays on Rochdale via Oldham line\nGood service on all other lines",
-            $s->getStatusString(), "Minor delays, peak");    }
+    private function getMinorDelaysData()
+    {
+        return json_decode('{
+            "retrievalDate": "22:32:30",
+            "items": 
+            [
+                {
+                    "detail": "<p>Due to an act of vandalism we are currently experiencing minor delays on the East Didsbury line.</p><p>Services travelling in both directions between East Didsbury and Deansgate-Castlefield are affected.</p><p>This is due to earlier Anti-social behaviour at East Didsbury.</p><p>Metrolink apologises for any inconvenience this may cause.</p>",
+                    "id": 4,
+                    "name": "East Didsbury",
+                    "severity": "warning",
+                    "status": "Minor delays"
+                },
+                {
+                    "detail": "<p>Due to an act of vandalism we are currently experiencing minor delays on the Rochdale via Oldham line.</p><p>Services travelling in both directions between Victoria and Rochdale Town Centre are affected.</p><p>Metrolink apologises for any inconvenience this may cause.</p>",
+                    "id": 8,
+                    "name": "Rochdale via Oldham",
+                    "severity": "warning",
+                    "status": "Minor delays"
+                },
+                {
+                    "name": "Other lines",
+                    "severity": "success",
+                    "status": "Good service"
+                }
+            ]
+        }');
+    }
+
+    private function getGoodServiceData()
+    {
+        return json_decode('{
+            "retrievalDate": "22:32:30",
+            "items": 
+            [
+                {
+                    "name": "All lines",
+                    "severity": "success",
+                    "status": "Good service"
+                }
+            ]
+        }');
+    }
 }

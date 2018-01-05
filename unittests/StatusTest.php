cf65de89b768bf07f688969c71d0879b7a3f209b
Daniel Shaw <daniel@shaw.works>
2018-01-05 18:00:27 +0000

    Move the data provider to a new class

--- unittests/StatusTest.php
+++ unittests/StatusTest.php
@@ -4,6 +4,7 @@ require_once('config/config.dist.php');
 require_once 'class/metrolink.class.php';
 require_once 'class/status.class.php';
 require_once 'class/promises/websitehandler.interface.php';
+require_once 'unittests/dataproviders/status.dataprovider.php';
 
 /**
  * @covers Metrolink
@@ -13,7 +14,7 @@ final class StatusTest extends TestCase
     public function setUp() {
     }
 
-    /** @dataProvider getStatusStringsProvider */
+    /** @dataProvider Dataprovider\Status::getStatusStringsProvider */
     public function testGetStatusString($datetime, $expected, $json)
     {
         $website = $this->createMock(WebsiteHandlerInterface::class);
@@ -22,75 +23,5 @@ final class StatusTest extends TestCase
         $metrolink = new Metrolink($datetime);
         $s = new Status($website, $metrolink);
         $this->assertEquals($expected, $s->getStatusString());
-    }
-
-    public function getStatusStringsProvider()
-    {
-        return [
-            'Good Service on all lines, non-peak' => [
-                new DateTimeImmutable('Monday 13:00'),
-                '',
-                $this->getGoodServiceData(),
-            ],
-            'Good service on all lines, peak' => [
-                new DateTimeImmutable('Monday 9:00'),
-                '9:00 — Good service on all lines',
-                $this->getGoodServiceData(),
-            ],
-            'Minor delays, non-peak' => [
-                new DateTimeImmutable('Monday 13:00'),
-                "Minor delays on East Didsbury line\nMinor delays on Rochdale via Oldham line\nGood service on all other lines",
-                $this->getMinorDelaysData(),
-            ],
-            'Minor delays, peak' => [
-                new DateTimeImmutable('Monday 17:00'),
-                "17:00 — Minor delays on East Didsbury line\nMinor delays on Rochdale via Oldham line\nGood service on all other lines",
-                $this->getMinorDelaysData(),
-            ],
-        ];
-    }
-
-    private function getMinorDelaysData()
-    {
-        return json_decode('{
-            "retrievalDate": "22:32:30",
-            "items": 
-            [
-                {
-                    "detail": "<p>Due to an act of vandalism we are currently experiencing minor delays on the East Didsbury line.</p><p>Services travelling in both directions between East Didsbury and Deansgate-Castlefield are affected.</p><p>This is due to earlier Anti-social behaviour at East Didsbury.</p><p>Metrolink apologises for any inconvenience this may cause.</p>",
-                    "id": 4,
-                    "name": "East Didsbury",
-                    "severity": "warning",
-                    "status": "Minor delays"
-                },
-                {
-                    "detail": "<p>Due to an act of vandalism we are currently experiencing minor delays on the Rochdale via Oldham line.</p><p>Services travelling in both directions between Victoria and Rochdale Town Centre are affected.</p><p>Metrolink apologises for any inconvenience this may cause.</p>",
-                    "id": 8,
-                    "name": "Rochdale via Oldham",
-                    "severity": "warning",
-                    "status": "Minor delays"
-                },
-                {
-                    "name": "Other lines",
-                    "severity": "success",
-                    "status": "Good service"
-                }
-            ]
-        }');
-    }
-
-    private function getGoodServiceData()
-    {
-        return json_decode('{
-            "retrievalDate": "22:32:30",
-            "items": 
-            [
-                {
-                    "name": "All lines",
-                    "severity": "success",
-                    "status": "Good service"
-                }
-            ]
-        }');
     }
 }
